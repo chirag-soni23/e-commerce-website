c4c6d48f0de2da1eafd052bc35dc3e0d1ac85ce8
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2023-11-21 15:36:26 +0530

    e-commerce

--- src/pages/registration/Login.jsx
+++ src/pages/registration/Login.jsx
@@ -1,14 +1,14 @@
-import { useContext, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import myContext from '../../context/data/myContext'
+import { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../fireabase/FirebaseConfig';
 import { toast } from 'react-toastify';
 import Loader from '../../components/loader/Loader';
+import myContext from '../../context/data/myContext';
 
 function Login() {
-    const context = useContext(myContext)
-    const {loading, setLoading} = context;
+    const context = useContext(myContext);
+    const { loading, setLoading } = context;
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -16,41 +16,70 @@ function Login() {
     const navigate = useNavigate();
 
     const login = async () => {
-        setLoading(true)
-        try {
-            const result = await signInWithEmailAndPassword(auth,email,password);
-            toast.success("Login successful", {
-                position: "top-right",
+        setLoading(true);
+
+        // Check if email and password are entered
+        if (!email || !password) {
+            toast.error('Please enter both email and password', {
+                position: 'top-right',
                 autoClose: 2000,
                 hideProgressBar: true,
                 closeOnClick: true,
                 pauseOnHover: true,
                 draggable: true,
                 progress: undefined,
-                theme: "colored",
-              })
-            localStorage.setItem('user', JSON.stringify(result))
-            navigate('/')
-            setLoading(false)
-            
-        } catch (error) {
-            console.log(error)
-            setLoading(loading)
+                theme: 'colored',
+            });
+            setLoading(false);
+            return;
         }
 
-    }
-   
+        try {
+            const result = await signInWithEmailAndPassword(auth, email, password);
+            toast.success('Login successful', {
+                position: 'top-right',
+                autoClose: 2000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: 'colored',
+            });
+            localStorage.setItem('user', JSON.stringify(result));
+            navigate('/');
+            setLoading(false);
+        } catch (error) {
+            console.error(error);
+
+            // Show an alert popup for invalid email/password
+            toast.error('Invalid email or password', {
+                position: 'top-right',
+                autoClose: 2000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: 'colored',
+            });
+
+            setLoading(false);
+        }
+    };
+
     return (
         <div className=' flex justify-center items-center h-screen'>
-            {loading && <Loader/>}
+            {loading && <Loader />}
             <div className=' bg-gray-800 px-10 py-10 rounded-xl '>
-                <div className="">
+                <div className=''>
                     <h1 className='text-center text-white text-xl mb-4 font-bold'>Login</h1>
                 </div>
                 <div>
-                    <input type="email"
-                    value={email}
-                    onChange={(e)=> setEmail(e.target.value)}
+                    <input
+                        type='email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         name='email'
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Email'
@@ -58,26 +87,31 @@ function Login() {
                 </div>
                 <div>
                     <input
-                        type="password"
+                        type='password'
                         value={password}
-                        onChange={(e)=> setPassword(e.target.value)}
+                        onChange={(e) => setPassword(e.target.value)}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Password'
                     />
                 </div>
                 <div className=' flex justify-center mb-3'>
                     <button
-                    onClick={login}
+                        onClick={login}
                         className=' bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg'>
                         Login
                     </button>
                 </div>
                 <div>
-                    <h2 className='text-white'>Don't have an account <Link className=' text-yellow-500 font-bold' to={'/signup'}>Signup</Link></h2>
+                    <h2 className='text-white'>
+                        Don't have an account{' '}
+                        <Link className=' text-yellow-500 font-bold' to={'/signup'}>
+                            Signup
+                        </Link>
+                    </h2>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Login
+export default Login;
