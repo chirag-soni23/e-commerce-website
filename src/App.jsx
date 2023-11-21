b9cc55a7310607f562b93e5aa6a573e93829c661
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2023-11-21 17:02:16 +0530

    e-commerce

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ export const ProtectedRoute = ({children}) => {
 const ProtectedRouteForAdmin = ({children})=> {
   const admin = JSON.parse(localStorage.getItem('user'))
   
-  if(admin.user.email === 'knupadhyay784@gmail.com'){
+  if(admin.user.email === 'csoni0692@gmail.com'){
     return children
   }
   else{
