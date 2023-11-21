c206f4696d96917ed925761753a2c57aac9afe63
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2023-11-21 20:43:24 +0530

    add

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -61,7 +61,7 @@ function Filter() {
             <button
               onClick={resetFilters}
               className="px-4 py-2 bg-gray-50 hover:bg-gray-200 text-gray-800 text-sm font-medium rounded-md"
-              style={{ color: mode === "dark" ? "white" : "" }}
+              style={{ color: mode === "dark" ? "dark" : "" }}
             >
               Reset Filter
             </button>
